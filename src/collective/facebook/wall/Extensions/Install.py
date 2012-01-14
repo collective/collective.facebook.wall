18e785ba11ee3040d60f83a0e152be5ba4b3b56a
Franco Pellegrini <frapell@gmail.com>
2012-01-14 03:10:55 -0300

    Added an initial profile so it will call the initial profile from collective.facebook.accounts

--- src/collective/facebook/wall/Extensions/Install.py
+++ src/collective/facebook/wall/Extensions/Install.py
@@ -1,6 +1,15 @@
 from Products.CMFCore.utils import getToolByName
 
-def uninstall(portal):
+def install(portal, reinstall=False):
     setup_tool = getToolByName(portal, 'portal_setup')
-    setup_tool.runAllImportStepsFromProfile('profile-collective.facebook.wall:uninstall')
+    if not reinstall:
+        setup_tool.runAllImportStepsFromProfile('profile-collective.facebook.wall:initial')
+
+    setup_tool.runAllImportStepsFromProfile('profile-collective.facebook.wall:default')
     return "Ran all uninstall steps."
+    
+def uninstall(portal, reinstall=False):
+    if not reinstall:
+        setup_tool = getToolByName(portal, 'portal_setup')
+        setup_tool.runAllImportStepsFromProfile('profile-collective.facebook.wall:uninstall')
+        return "Ran all uninstall steps."
